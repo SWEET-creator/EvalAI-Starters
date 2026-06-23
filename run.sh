3e083379ab28dea6e98b1fe234eb5f26296193b5
SWEET-creator <sosui0131@icloud.com>
2026-06-23 14:21:44 +0000

    fix

--- run.sh
+++ run.sh
@@ -30,7 +30,14 @@ with ZipFile("evaluation_script.zip", "w", ZIP_DEFLATED) as zf:
         add_file(zf, path, Path(path).relative_to("evaluation_script"))
 
 with ZipFile("challenge_config.zip", "w", ZIP_DEFLATED) as zf:
-    for path in ["challenge_config.yaml", "evaluation_script.zip", "logo.jpg", "submission.json"]:
+    for path in [
+        "challenge_config.yaml",
+        "evaluation_script.zip",
+        "logo.jpg",
+        "submission.json",
+        "f034078f-3fc8-4a9e-a6c7-8dfeb04b9102.json",
+        "936aa32e-760f-4ada-8672-ee4bfeeb5d05.json",
+    ]:
         add_file(zf, path)
     for root in ["annotations", "templates"]:
         add_tree(zf, root)
